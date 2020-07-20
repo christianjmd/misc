8f272ee76dd8a30c7abbb3eb8eeb701fa5fe85c6
Christian Dinglasan <65063750+christianjmd@users.noreply.github.com>
2020-07-20 23:01:03 +1000

    Add files via upload

---
+++ cool.py
@@ -0,0 +1,68 @@
+#printing statements
+print("hello world")
+print(1+1)
+
+#variables is container
+x = 5
+potato = 65
+
+data types
+one = 5 # whole number
+two = "potato" #strings
+three = 5.123 #float (number with decimal point)
+
+# if/else statements
+
+christian = "cool" #one = means that it's assigning something
+andre = "lame"
+poo = 2
+
+if (12>1):
+    print("True")
+else:
+    print("False")
+
+# loops
+
+output = 10
+
+while output != 0:
+    print(output)
+    output -= 1
+
+for (counter) in range (initialisation, condition, updation):
+
+
+
+# functions
+
+def add(number1, number2):
+    print(number1 + number2)
+
+add(5, 7)
+
+def welcome(name):
+    print("Welcome " + name) # concetenation (joining)
+
+welcome("12")
+
+
+def input(grade):
+
+    if (grade > 0) and (grade <= 100):
+
+        if (grade > 0) and (grade<=49):
+            print("you killed it")
+
+        if
+
+    else:
+        print("invalid input")
+
+
+
+input("poo")
+
+
+
+
